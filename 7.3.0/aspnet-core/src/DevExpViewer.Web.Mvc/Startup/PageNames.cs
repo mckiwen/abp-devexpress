b305f68f90a47736ff9d809b3eff2adea0cfc88a
Alberto <albantunez91@gmail.com>
2023-03-28 22:24:43 +0100

    Reporting Viewer insertado en About Page con algunos pequeños errores

--- 7.3.0/aspnet-core/src/DevExpViewer.Web.Mvc/Startup/PageNames.cs
+++ 7.3.0/aspnet-core/src/DevExpViewer.Web.Mvc/Startup/PageNames.cs
@@ -3,7 +3,7 @@
     public class PageNames
     {
         public const string Home = "Home";
-        public const string About = "About";
+        public const string About = "Report Viewer";
         public const string Tenants = "Tenants";
         public const string Users = "Users";
         public const string Roles = "Roles"; 
